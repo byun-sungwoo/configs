1c62907bbae476c991b822f6e768c9fb8b213edc
byun-sungwoo <dbyun1999@gmail.com>
2019-11-24 14:20:08 -0600

    config update

---
+++ ubuntu/.my_commands.sh
@@ -0,0 +1,50 @@
+# update functions in bashrc
+function update-bashrc() {
+	source ~/.bashrc
+}
+
+# ssh to burrow as dsbyun
+function dsbyun@burrow() {
+	ssh dsbyun@burrow.soic.indiana.edu
+}
+
+# ssh to silo as dsbyun
+function dsbyun@silo() {
+	ssh dsbyun@silo.soic.indiana.edu
+}
+
+PATH1=~/Desktop/2019-fall/c212_ta/attendance
+
+# [C212 Attendance Program]
+# Compile and run attendance program. Move files.
+function lab212-runmv() {
+        og_pwd=$(pwd) # store to go back later
+        cd $PATH1
+        mv output/newlab/* output/oldlabs/
+        javac Attendance.java
+        java Attendance
+        mv output/!(oldlabs|newlab) output/newlab
+        cd $og_pwd # go back to og
+}
+
+# Compile and run attendance program.
+function lab212-run() {
+	og_pwd=$(pwd)
+	cd $PATH1
+	javac Attendance.java
+	java Attendance
+	cd $og_pwd
+}
+
+# Open first file in newlab
+function lab212-open() {
+        og_pwd=$(pwd)
+        cd $PATH1/output/newlab
+        vim $(ls *|head -n 1)
+        cd $og_pwd
+}
+
+# Call the long ass gogh command
+function gogh-color() {
+	bash -c "$(wget -qO- https://git.io/vQgMr)"
+}
---
+++ vim/.vimrc
@@ -0,0 +1,29 @@
+" Mute bell sound
+set visualbell
+
+" Mute visual flash
+set t_vb=
+
+" Show current line
+" set cursorline
+
+" Show line numbers
+set number
+
+" Show tabs
+set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<
+set list
+
+" Disable Arrow Keys
+nnoremap <Left> :echo "that's an arrow key"<CR>
+vnoremap <Left> :<C-u>echo "that's an arrow key"<CR>
+inoremap <Left> <C-o>:echo "that's an arrow key"<CR>
+nnoremap <Right> :echo "that's an arrow key"<CR>
+vnoremap <Right> :<C-u>echo "that's an arrow key"<CR>
+inoremap <Right> <C-o>:echo "that's an arrow key"<CR>
+nnoremap <Up> :echo "that's an arrow key"<CR>
+vnoremap <Up> :<C-u>echo "that's an arrow key"<CR>
+inoremap <Up> <C-o>:echo "that's an arrow key"<CR>
+nnoremap <Down> :echo "that's an arrow key"<CR>
+vnoremap <Down> :<C-u>echo "that's an arrow key"<CR>
+inoremap <Down> <C-o>:echo "that's an arrow key"<CR>
